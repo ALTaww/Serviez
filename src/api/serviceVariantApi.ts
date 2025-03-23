dd31781d4f43cda0bfc916a7b09300c26ed7ab05
ALTaww <123657665+ALTaww@users.noreply.github.com>
2025-03-24 02:24:17 +0300

    add serviceVariantApi

--- src/api/serviceVariantApi.ts
+++ src/api/serviceVariantApi.ts
@@ -1,3 +1,72 @@
-class ServiceVariantApi {}
+import { $serviceVariantHost } from ".";
+import { IServiceVariant, IWorkingHours } from "../types/database";
+
+class ServiceVariantApi {
+  create = async (
+    serviceId: string,
+    variant: Omit<IServiceVariant, "_id" | "service">,
+    signal: AbortSignal
+  ): Promise<IServiceVariant> => {
+    const { data } = await $serviceVariantHost.post(
+      `/${serviceId}/variants`,
+      variant,
+      { signal }
+    );
+    return data;
+  };
+
+  createWithWorkingHours = async (
+    serviceId: string,
+    variant: Omit<IServiceVariant, "_id" | "service">,
+    workingHours: IWorkingHours["days"],
+    signal: AbortSignal
+  ): Promise<IServiceVariant> => {
+    const { data } = await $serviceVariantHost.post(
+      `/${serviceId}/variants`,
+      { variant, workingHours },
+      { signal }
+    );
+    return data;
+  };
+
+  getVariantsByServiceId = async (
+    serviceId: string,
+    signal: AbortSignal
+  ): Promise<IServiceVariant[]> => {
+    const { data } = await $serviceVariantHost.get(`/${serviceId}`, { signal });
+    return data;
+  };
+
+  getAllVariants = async (signal: AbortSignal): Promise<IServiceVariant[]> => {
+    const { data } = await $serviceVariantHost.get(`/`, { signal });
+    return data;
+  };
+
+  update = async (
+    serviceId: string,
+    variantId: string,
+    variant: Omit<IServiceVariant, "_id" | "service">,
+    signal: AbortSignal
+  ): Promise<IServiceVariant[]> => {
+    const { data } = await $serviceVariantHost.put(
+      `/${serviceId}/${variantId}`,
+      variant,
+      { signal }
+    );
+    return data;
+  };
+
+  delete = async (
+    serviceId: string,
+    variantId: string,
+    signal: AbortSignal
+  ): Promise<IServiceVariant[]> => {
+    const { data } = await $serviceVariantHost.delete(
+      `/${serviceId}/${variantId}`,
+      { signal }
+    );
+    return data;
+  };
+}
 
 export default new ServiceVariantApi();
