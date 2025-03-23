bf2c3007d09dcd7a286af90bbbd3f6cb73458c6c
ALTaww <123657665+ALTaww@users.noreply.github.com>
2025-03-23 18:30:02 +0300

    add database interfaces

--- src/types/database.ts
+++ src/types/database.ts
@@ -1,15 +1,52 @@
-export interface IUsers {
-  id: number;
+export interface IUser {
+  _id: string;
   name: string;
   surname: string;
   email: string;
+  role: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+export interface IService {
+  _id: string;
+  title: string;
+  owner: string;
+  description: string;
+  variants: string[];
+  isActive: boolean;
+  category: string;
 }
 
-export interface IServices {
-  id: number;
-  creator_id: number;
-  name: string;
-  price: number;
+export interface IServiceVariant {
+  _id: string;
+  employee: string;
+  title: string;
   description: string;
-  time_doing_min: number;
+  duration: number; // in minutes
+  price: number; // rubles
+  service: string;
+  workingHours?: IWorkingHours;
+}
+
+export interface IWorkingHours {
+  _id: string;
+  variant: string;
+  days: {
+    start: Date;
+    end: Date;
+    breaks: {
+      start: Date;
+      end: Date;
+    }[];
+  }[];
+}
+export interface IBooking {
+  _id: string;
+  client: string;
+  employee: string;
+  service: string;
+  serviceVariant: string;
+  start: Date;
+  end: Date;
+  status: string;
 }
